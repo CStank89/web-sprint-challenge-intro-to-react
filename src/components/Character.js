13098affe02f6ea6cfbb4d14c24a8df8dcd84929
CStank89 <C.Stank89@gmail.com>
2020-09-11 13:05:48 -0400

    I hit the limit trying all these styling out

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react';
+
+
+
+const Character = (props) => {
+    const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld, films } = props.character
+
+
+
+    return (
+        
+    
+        <div className="container">
+             <h2>{name}</h2>
+             <p>{height}</p>
+             <p>{mass}</p>
+             <p>{hair_color}</p>
+             <p>{skin_color}</p>
+             <p>{eye_color}</p>
+             <p>{birth_year}</p>
+             <p>{gender}</p>
+             <p>{homeworld}</p>
+             <p>{films}</p>
+         </div>
+
+    )
+
+}
+export default Character;
